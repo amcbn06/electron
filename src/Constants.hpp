b644f999a077af89ca66195d01adc05adb646428
tibinyte <tibinyte2006@gmail.com>
2025-12-30 09:18:28 +0200

    fix bugs

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -9,4 +9,7 @@ namespace Constants {
     
     // #TODO: idk how to implement panning sensitivity
     inline const float panningSensitivity = 1.0f;
+
+    const float zoomAlpha = 0.5;
+    const long double paddingDelay = 0.07; // in seconds
 }
